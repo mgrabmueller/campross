a3618256c1c51b2162be774dd91030324f233c6d
Martin Grabmueller <martin@grabmueller.de>
2016-10-24 23:55:05 +0200

    Range coder experiments, static Huffman implementation, baby steps.

--- src/huff.rs
+++ src/huff.rs
@@ -2,30 +2,236 @@
 // top-level directory of this distribution for license information.
 
 //! Simple implementation of a Huffman encoder.
+//!
+//! Based on the static Huffman encoder in Mark Nelson, Jean-Loup
+//! Gailly: The Data Compression Book, 2nd Edition, M&T Books, 1996.
 
 use std::io::{Read, Write};
 use std::io;
 use error::Error;
 use bitfile::{BitWriter, BitReader};
 
+const BLOCK_SIZE: usize = 1024 * 32;
+const EOB: usize = 256;
+const EOF: usize = 257;
+
+const MAX_COUNT: usize = 0x1fff;
+
+type Symbol = u32;
+
+#[derive(Clone, Copy)]
+struct Node {
+    weight: usize,
+    child0: usize,
+    child1: usize,
+    parent: usize,
+    active: bool,
+}
 
 pub struct CompressWriter<W> {
-    inner: W,
+    inner: BitWriter<W>,
+    block: [u8; BLOCK_SIZE],
+    fill:  usize,
+    freqs: [usize; EOF + 1],
+    tree:  [Node; 2 * (EOF + 1) + 1],
+    codes: [(u64, usize); EOF + 1],
 }
 
 impl<W: Write> CompressWriter<W> {
     pub fn new(inner: W) -> CompressWriter<W>{
         CompressWriter {
-            inner: inner,
+            inner: BitWriter::new(inner),
+            block: [0; BLOCK_SIZE],
+            fill:  0,
+            freqs: [0; EOF + 1],
+            tree: [Node{weight: 0, child0: 0, child1: 0, parent: 0, active: false};
+                   2 * (EOF + 1) + 1],
+            codes: [(0, 0); EOF + 1],
         }
     }
 
+    fn count_freqs(&mut self) {
+        for i in 0..EOB {
+            self.freqs[i] = 0;
+        }
+        self.freqs[EOF] = 1;
+        self.freqs[EOB] = 1;
+        for i in 0..self.fill {
+            self.freqs[self.block[i] as usize] += 1;
+        }
+        let mut max_count = 0;
+        for i in 0..EOF {
+            if self.freqs[i] > max_count {
+                max_count = self.freqs[i];
+            }
+        }
+        while max_count > MAX_COUNT {
+            max_count = 0;
+            for i in 0..EOF {
+                self.freqs[i] = (self.freqs[i] + 1) / 2;
+            }
+            for i in 0..EOF {
+                if self.freqs[i] > max_count {
+                    max_count = self.freqs[i];
+                }
+            }
+        }
+    }
+
+    fn build_tree(&mut self) -> usize {
+        for i in 0..EOF + 1 {
+            if self.freqs[i] > 0 {
+                self.tree[i].weight = self.freqs[i];
+                self.tree[i].active = true;
+            }
+        }
+        let last_node_idx = 2 * (EOF + 1);
+        self.tree[last_node_idx].weight = 0xffff;
+        let mut next_free = EOF + 1;
+        loop {
+            let mut min1 = last_node_idx;
+            let mut min2 = last_node_idx;
+
+            for i in 0..next_free {
+                if self.tree[i].active {
+                    if self.tree[i].weight < self.tree[min1].weight {
+                        min2 = min1;
+                        min1 = i;
+                    } else if self.tree[i].weight < self.tree[min2].weight {
+                        min2 = i;
+                    }
+                }
+            }
+//            println!("min1 {} min2 {}", min1, min2);
+            if min2 == last_node_idx {
+                break;
+            }
+            self.tree[next_free].weight =
+                self.tree[min1].weight + self.tree[min2].weight;
+            self.tree[next_free].active = true;
+            self.tree[min1].active = false;
+            self.tree[min2].active = false;
+            self.tree[min1].parent = next_free;
+            self.tree[min2].parent = next_free;
+            self.tree[next_free].child0 = min1;
+            self.tree[next_free].child1 = min2;
+            next_free += 1;
+        }
+        next_free - 1
+    }
+
+    fn dump_freqs(&self) {
+        for i in 0..EOF + 1 {
+            if self.freqs[i] > 0 {
+                match i {
+                    EOB => println!("EOB: {}", self.freqs[i]),
+                    EOF => println!("EOF: {}", self.freqs[i]),
+                    _ =>
+                        println!("{:3} {:?}: {}", i, (i as u8) as char, self.freqs[i]),
+                }
+            }
+        }
+    }
+    
+    fn dump_tree(&self, root: usize, indent: usize) {
+        for _ in 0..indent {
+            print!(" ");
+        }
+        if root <= EOF {
+            match root {
+                EOB => println!("EOB"),
+                EOF => println!("EOF"),
+                _ if root < 256 => println!("{} {:?}", root, (root as u8) as char),
+                _ => println!("{}", root),
+            }
+        } else {
+            println!("{}", root);
+            self.dump_tree(self.tree[root].child0, indent + 1);
+            self.dump_tree(self.tree[root].child1, indent + 1);
+        }
+    }
+
+    fn calc_code(&self, root: usize, sym: Symbol) -> (u64, usize) {
+        let mut node = sym as usize;
+        let mut code = 0u64;
+        let mut code_len = 0;
+        while node != root {
+            code >>= 1;
+            if self.tree[self.tree[node].parent].child1 == node {
+                code |= 1 << 63;
+            }
+            code_len += 1;
+            node = self.tree[node].parent;
+        }
+        code >>= 64 - code_len;
+        assert!(code_len < 20);
+        (code, code_len)
+    }
+
+    fn calc_codes(&mut self, root: usize) {
+        for i in 0..EOF+1 {
+            if self.tree[i].weight > 0 {
+                self.codes[i] = self.calc_code(root, i as Symbol);
+            }
+        }
+    }
+    
+    fn process_block(&mut self, final_block: bool) -> io::Result<()> {
+        println!("");
+        self.count_freqs();
+        self.dump_freqs();
+        let root = self.build_tree();
+        self.dump_tree(root, 0);
+
+        self.calc_codes(root);
+
+        let mut sym_cnt = 0;
+        for i in 0..EOF + 1 {
+            if self.freqs[i] > 0 {
+                sym_cnt += 1;
+            }
+        }
+        try!(self.inner.write_bits(sym_cnt as u64, 9));
+        for i in 0..EOB {
+            if self.freqs[i] > 0 {
+                try!(self.inner.write_bits(i as u64, 9));
+                try!(self.inner.write_bits(self.freqs[i] as u64, 16));
+            }
+        }
+        for i in 0..self.fill {
+            let c = self.block[i];
+            let (code, code_len) = self.codes[c as usize];
+            try!(self.inner.write_bits(code, code_len));
+            println!("{:032b} {}", code, code_len);
+        }
+        let marker = if final_block { EOF } else { EOB };
+        let (code, code_len) = self.codes[marker as usize];
+        try!(self.inner.write_bits(code, code_len));
+        println!("{:032b} {}", code, code_len);
+        self.fill = 0;
+        Ok(())
+    }
+    
     fn process(&mut self, input: &[u8]) -> io::Result<usize> {
-        Ok(0)
+        let mut input_ptr = 0;
+        while input_ptr < input.len() {
+            let space = BLOCK_SIZE - self.fill;
+            let cp = ::std::cmp::min(space, input.len() - input_ptr);
+            for i in 0..cp {
+                self.block[self.fill + i] = input[input_ptr + i];
+            }
+            self.fill += cp;
+            input_ptr += 1;
+            if self.fill == BLOCK_SIZE {
+                try!(self.process_block(false));
+            }
+        }
+        
+        Ok(input_ptr)
     }
 
     pub fn to_inner(self) -> W {
-        self.inner
+        self.inner.to_inner()
     }
 }
 
@@ -38,23 +244,126 @@ impl<W: Write> Write for CompressWriter<W> {
     /// written data to be compressed and written to the underlying
     /// Writer, which is also flushed.
     fn flush(&mut self) -> io::Result<()> {
+        try!(self.process_block(true));
         self.inner.flush()
     }
 }
 
 pub struct DecompressReader<R> {
-    inner: R,
+    inner: BitReader<R>,
+    freqs: [usize; EOF + 1],
+    tree:  [Node; 2 * (EOF + 1) + 1],
+    in_block: bool,
+    eof: bool,
 }
 
 impl<R: Read> DecompressReader<R> {
     pub fn new(inner: R) -> DecompressReader<R> {
         DecompressReader {
-            inner: inner,
+            inner: BitReader::new(inner),
+            freqs: [0; EOF + 1],
+            tree: [Node{weight: 0, child0: 0, child1: 0, parent: 0, active: false};
+                   2 * (EOF + 1) + 1],
+            in_block: false,
+            eof: false,
         }
     }
 
+    fn build_tree(&mut self) -> usize {
+        for i in 0..EOF + 1 {
+            if self.freqs[i] > 0 {
+                self.tree[i].weight = self.freqs[i];
+                self.tree[i].active = true;
+            } else {
+                self.tree[i].active = false;
+            }
+        }
+        let last_node_idx = 2 * (EOF + 1);
+        self.tree[last_node_idx].weight = 0xffff;
+        let mut next_free = EOF + 1;
+        loop {
+            let mut min1 = last_node_idx;
+            let mut min2 = last_node_idx;
+
+            for i in 0..next_free {
+                if self.tree[i].active {
+                    if self.tree[i].weight < self.tree[min1].weight {
+                        min2 = min1;
+                        min1 = i;
+                    } else if self.tree[i].weight < self.tree[min2].weight {
+                        min2 = i;
+                    }
+                }
+            }
+//            println!("min1 {} min2 {}", min1, min2);
+            if min2 == last_node_idx {
+                break;
+            }
+            self.tree[next_free].weight =
+                self.tree[min1].weight + self.tree[min2].weight;
+            self.tree[next_free].active = true;
+            self.tree[min1].active = false;
+            self.tree[min2].active = false;
+            self.tree[min1].parent = next_free;
+            self.tree[min2].parent = next_free;
+            self.tree[next_free].child0 = min1;
+            self.tree[next_free].child1 = min2;
+            next_free += 1;
+        }
+        next_free - 1
+    }
+
+    fn decode(&mut self, root: usize) -> io::Result<Symbol> {
+        let mut n = root;
+        while n > EOF {
+            let b = try!(self.inner.read_bits(1)) as usize;
+            if b == 0 {
+                n = self.tree[n].child0;
+            } else {
+                n = self.tree[n].child1;
+            }
+        }
+        Ok(n as Symbol)
+    }
+    
     fn process(&mut self, output: &mut [u8]) -> io::Result<usize> {
-        Ok(0)
+        let mut root = 0;
+        
+        if self.eof {
+            return Ok(0);
+        }
+        
+        let mut written = 0;
+        'outer:
+        loop {
+            if !self.in_block {
+                let sym_count = try!(self.inner.read_bits(9)) as usize;
+                for _ in 0..sym_count {
+                    let sym = try!(self.inner.read_bits(9));
+                    let freq = try!(self.inner.read_bits(16));
+                    self.freqs[sym as usize] = freq as usize;
+                }
+                self.freqs[EOB] = 1;
+                self.freqs[EOF] = 1;
+                root = self.build_tree();
+                self.in_block = true;
+            }
+            'inner:
+            for i in written..output.len() {
+                let b = try!(self.decode(root));
+                if b as usize == EOF {
+                    self.eof = true;
+                    break 'outer;
+                } else if b as usize == EOB {
+                    self.in_block = false;
+                    break 'inner;
+                } else {
+                    output[i] = b as u8;
+                    written += 1;
+                }
+            }
+        }
+        Ok(written)
     }
 }
 
@@ -80,14 +389,17 @@ pub fn decompress<R: Read, W: Write>(input: R, mut output: W) -> Result<W, Error
 
 #[cfg(test)]
 mod test {
-    use ::std::io::Cursor;
-    use super::{compress}; //, decompress};
+    use ::std::io::Write;
+    use super::{CompressWriter}; //, decompress};
     
     #[test]
     fn compress_empty() {
-        let input = [];
-        let compressed = compress(Cursor::new(&input[..]), vec![]).unwrap();
-        let expected = [0u8];
+        let input = b"";
+        let mut cw = CompressWriter::new(vec![]);
+        cw.write(&input[..]).unwrap();
+        cw.flush().unwrap();
+        let compressed = cw.to_inner();
+        let expected = [1, 64];
         println!("compressed: {:?}", compressed);
         assert_eq!(&expected[..], &compressed[..]);
     }
@@ -95,8 +407,11 @@ mod test {
     #[test]
     fn compress_a() {
         let input = b"a";
-        let compressed = compress(Cursor::new(&input[..]), vec![]).unwrap();
-        let expected = [0u8];
+        let mut cw = CompressWriter::new(vec![]);
+        cw.write(&input[..]).unwrap();
+        cw.flush().unwrap();
+        let compressed = cw.to_inner();
+        let expected = [1, 152, 64, 0, 96];
         println!("compressed: {:?}", compressed);
         assert_eq!(&expected[..], &compressed[..]);
     }
--- src/range.rs
+++ src/range.rs
@@ -1,19 +1,250 @@
+// Copyright 2016 Martin Grabmueller. See the LICENSE file at the
+// top-level directory of this distribution for license information.
 
+//! Adapted from https://github.com/kazuho/rangecoder/blob/master/range_coder.hpp
 
-type Symbol = u16;
+use std::io::{Read, Write};
+use std::num::Wrapping;
 
-const MIN: u64 = 0;
-const MAX: u64 = 0x1000_0000_0000_0000;
+const TOP: u32 = 1 << 24;
 
-const EOF: Symbol = 256;
-const ESC: Symbol = 257;
-const FLUSH: Symbol = 258;
+pub struct RangeCoder<W> {
+    ll: u32,
+    rr: u32,
+    buffer: u8,
+    start: bool,
+    carry_n: usize,
+    counter: usize,
+    inner: W,
+}
 
-pub fn encode(input: &[u8], output: Vec<u8>) {
-    let ranges =
-        [
-            (EOF, 1),
-            (ESC, 2),
-            (FLUSH, 1)
-        ];
+impl<W: Write> RangeCoder<W> {
+    pub fn new(output: W) -> RangeCoder<W> {
+        RangeCoder {
+            ll: 0,
+            rr: 0xffffffff,
+            buffer: 0,
+            carry_n: 0,
+            counter: 0,
+            start: true,
+            inner: output,
+        }
+    }
+    
+    pub fn encode(&mut self, symbol: u32, cum_freq: &[u32]) {
+        let lo = cum_freq[symbol as usize];
+        let hi = cum_freq[symbol as usize + 1];
+        let total = cum_freq[cum_freq.len() - 1];
+        let r = self.rr / total;
+        println!("lo={} hi={} total={} r={}", lo, hi, total, r);
+        if hi < total {
+            self.rr = r * (hi - lo);
+        } else {
+            self.rr -= r * lo;
+        }
+        println!("rr={:08x}", self.rr);
+        let new_ll = (Wrapping(self.ll) + Wrapping(r) * Wrapping(lo)).0;
+        if new_ll < self.ll {
+            self.buffer += 1;
+            while self.carry_n != 0 {
+                println!("{:2x}", self.buffer);
+                let buffer = self.buffer;
+                self.putc(buffer);
+                self.buffer = 0;
+                self.carry_n -= 1;
+            }
+        }
+        self.ll = new_ll;
+        println!("ll={:08x}", self.ll);
+        while self.rr < TOP {
+            let new_buffer = (self.ll > 24) as u8;
+            if self.start {
+                self.buffer = new_buffer;
+                self.start = false;
+            } else if new_buffer == 0xff {
+                self.carry_n += 1;
+            } else {
+                println!("{:2x}", self.buffer);
+                let buffer = self.buffer;
+                self.putc(buffer);
+                while self.carry_n != 0 {
+                    println!("{:2x}", 0xffu8);
+                    self.putc(0xff);
+                    self.carry_n -= 1;
+                }
+                self.buffer = new_buffer;
+            }
+            self.ll <<= 8;
+            self.rr <<= 8;
+        }
+        println!("final ll={:08x}", self.ll);
+        self.counter += 1;
+    }
+
+    pub fn finish(&mut self) {
+        let buffer = self.buffer;
+        self.putc(buffer);
+        println!("{:2x}", self.buffer);
+        while self.carry_n != 0 {
+            println!("{:2x}", 0xffu8);
+            self.putc(0xff);
+            self.carry_n -= 1;
+        }
+        let mut t = self.ll + self.rr;
+        loop {
+            let t8 = t >> 24;
+            let l8 = self.ll >> 24;
+            
+            self.putc(l8 as u8);
+            println!("{:2x}", l8);
+            
+            if t8 != l8 {
+	        break;
+            }
+            t <<= 8;
+            self.ll <<= 8;
+        }
+    }
+
+    fn putc(&mut self, b: u8) {
+        let nwritten = self.inner.write(&[b]).expect("could not write");
+        assert_eq!(1, nwritten);
+    }
+    
+    pub fn to_inner(self) -> W {
+        self.inner
+    }
+}
+
+pub struct RangeDecoder<R> {
+    rr: u32,
+    dd: u32,
+    inner: R,
+}
+
+impl<R: Read> RangeDecoder<R> {
+    pub fn new(input: R) -> RangeDecoder<R> {
+        let mut rd = RangeDecoder {
+            rr: 0xffffffff,
+            dd: 0,
+            inner: input,
+        };
+        for _ in 0..4 {
+            rd.dd = (rd.dd << 8) | (rd.next() as u32);
+        }
+        println!("dd primed: {:08x}", rd.dd);
+        rd
+    }
+
+    pub fn next(&mut self) -> u8 {
+        let mut buf = [0u8; 1];
+        let nread = self.inner.read(&mut buf[..]).expect("cannot read");
+        if nread == 1 {
+            buf[0]
+        } else {
+            0xff
+        }
+    }
+
+    pub fn decode(&mut self, cum_freq: &[u32]) -> u32 {
+        let total = cum_freq[cum_freq.len() - 1];
+        
+        println!("in: rr={:08x}, dd={:08x}", self.rr, self.dd);
+        
+        let r = self.rr / total;
+        
+        println!("r={} (total={}), dd/r={}", r, total, self.dd / r);
+        
+        let target_pos = ::std::cmp::min(total - 1, self.dd / r);
+        
+        println!("target_pos={}", target_pos);
+        
+        let mut index = 0;
+        for i in 0..cum_freq.len() {
+            if target_pos < cum_freq[i] {
+                index = i - 1;
+                break;
+            }
+        }
+        
+        println!("index={}", index);
+        
+        let lo = cum_freq[index];
+        
+        println!("lo={}", lo);
+        
+        let hi = cum_freq[index + 1];
+        
+        println!("hi={}", hi);
+
+        self.dd -= r * lo;
+        
+        println!("dd={:08x}", self.dd);
+        
+        if hi != total {
+            self.rr = r * (hi - lo);
+        } else {
+            self.rr -= r * lo;
+        }
+        
+        println!("rr={:08x}", self.rr);
+        
+        while self.rr < TOP {
+            self.rr <<= 8;
+            self.dd = (self.dd << 8) | (self.next() as u32);
+            
+            println!("reloaded rr={:08x}", self.rr);
+        }
+        
+        println!("out: symbol={}. rr={:08x}, dd={:08x}", index, self.rr, self.dd);
+        
+        index as u32
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::{RangeCoder, RangeDecoder};
+    use ::std::io::Cursor;
+
+    // Text: 0,1,0,2,3
+    // Symbols:   0,1,2,3
+    // Freq:      2,1,1,1
+    // Cum. Freq: 0,2,3,4,5
+    static CUM_FREQ: [u32; 5] = [0,1,2,3,4];
+
+    #[test]
+    fn encode_1() {
+        let mut rc = RangeCoder::new(vec![]);
+
+        rc.encode(0, &CUM_FREQ);
+        rc.encode(1, &CUM_FREQ);
+        rc.encode(0, &CUM_FREQ);
+        rc.encode(2, &CUM_FREQ);
+        rc.encode(3, &CUM_FREQ);
+        rc.finish();
+
+        let coded = rc.to_inner();
+        let expected = [0x01u8, 0x2f];
+        assert_eq!(&expected[..], &coded[..]);
+        assert!(false);
+   }
+
+    #[test]
+    fn decode_1() {
+        let mut rc = RangeDecoder::new(Cursor::new(vec![0x01, 0x2f]));
+
+        let c = rc.decode(&CUM_FREQ);
+        assert_eq!(0, c);
+        let c = rc.decode(&CUM_FREQ);
+        assert_eq!(1, c);
+        let c = rc.decode(&CUM_FREQ);
+        assert_eq!(0, c);
+        let c = rc.decode(&CUM_FREQ);
+        assert_eq!(2, c);
+        let c = rc.decode(&CUM_FREQ);
+        assert_eq!(3, c);
+
+        assert!(false);
+   }
 }
